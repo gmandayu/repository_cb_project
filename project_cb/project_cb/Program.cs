1ff3566659424e526bdc3d604559e22b5c8e513e
Guntur Mandayu <gmandayu@gmail.com>
2020-12-28 13:48:13 +0700

    Apply MetroFramework to project

--- project_cb/project_cb/Program.cs
+++ project_cb/project_cb/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace project_cb
+namespace project_cb.UI
 {
     static class Program
     {
@@ -16,7 +16,7 @@ namespace project_cb
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new form_login());
         }
     }
 }
